163bd6fbfd3651143763b25e7291278c7a94b013
Atrishman Mukherjee <atrishmanm@gmail.com>
2026-03-18 15:35:59 +0530

    fix

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class TrainApp {
+public class TrainConsistManagementApp {
     public static void main(String[] args) {
         System.out.println("==== UC2 - Add Passenger Bogies to Train ====");
         System.out.println("============================================\n");
