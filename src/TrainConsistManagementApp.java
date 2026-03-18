4805ad2c2e971e17d759915ddbe23ebbc062fb1c
Atrishman Mukherjee <atrishmanm@gmail.com>
2026-03-18 15:37:52 +0530

    fix

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class UseCase6TrainConsistMgmnt {
+public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
         System.out.println("==========================================");
